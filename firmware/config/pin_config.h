1da7ee5c97888b4057a3cafc701ddd5c8cb9a4b9
Sigmond Kukla <sigkukla@gmail.com>
2026-04-06 14:46:24 -0400

    Update pin config

--- firmware/config/pin_config.h
+++ firmware/config/pin_config.h
@@ -68,6 +68,30 @@
 // [PRS.SYNCH3]$
 
 // $[GPIO]
+// GPIO SWCLK on PA01
+#ifndef GPIO_SWCLK_PORT                         
+#define GPIO_SWCLK_PORT                          SL_GPIO_PORT_A
+#endif
+#ifndef GPIO_SWCLK_PIN                          
+#define GPIO_SWCLK_PIN                           1
+#endif
+
+// GPIO SWDIO on PA02
+#ifndef GPIO_SWDIO_PORT                         
+#define GPIO_SWDIO_PORT                          SL_GPIO_PORT_A
+#endif
+#ifndef GPIO_SWDIO_PIN                          
+#define GPIO_SWDIO_PIN                           2
+#endif
+
+// GPIO SWV on PA03
+#ifndef GPIO_SWV_PORT                           
+#define GPIO_SWV_PORT                            SL_GPIO_PORT_A
+#endif
+#ifndef GPIO_SWV_PIN                            
+#define GPIO_SWV_PIN                             3
+#endif
+
 // [GPIO]$
 
 // $[TIMER0]
@@ -86,6 +110,38 @@
 // [TIMER4]$
 
 // $[USART0]
+// USART0 CLK on PC05
+#ifndef USART0_CLK_PORT                         
+#define USART0_CLK_PORT                          SL_GPIO_PORT_C
+#endif
+#ifndef USART0_CLK_PIN                          
+#define USART0_CLK_PIN                           5
+#endif
+
+// USART0 CS on PC06
+#ifndef USART0_CS_PORT                          
+#define USART0_CS_PORT                           SL_GPIO_PORT_C
+#endif
+#ifndef USART0_CS_PIN                           
+#define USART0_CS_PIN                            6
+#endif
+
+// USART0 RX on PC03
+#ifndef USART0_RX_PORT                          
+#define USART0_RX_PORT                           SL_GPIO_PORT_C
+#endif
+#ifndef USART0_RX_PIN                           
+#define USART0_RX_PIN                            3
+#endif
+
+// USART0 TX on PC04
+#ifndef USART0_TX_PORT                          
+#define USART0_TX_PORT                           SL_GPIO_PORT_C
+#endif
+#ifndef USART0_TX_PIN                           
+#define USART0_TX_PIN                            4
+#endif
+
 // [USART0]$
 
 // $[I2C1]
@@ -101,6 +157,22 @@
 // [LETIMER0]$
 
 // $[IADC0]
+// IADC0 SCAN0POS on PA00
+#ifndef IADC0_SCAN0POS_PORT                     
+#define IADC0_SCAN0POS_PORT                      SL_GPIO_PORT_A
+#endif
+#ifndef IADC0_SCAN0POS_PIN                      
+#define IADC0_SCAN0POS_PIN                       0
+#endif
+
+// IADC0 SCAN1POS on PD00
+#ifndef IADC0_SCAN1POS_PORT                     
+#define IADC0_SCAN1POS_PORT                      SL_GPIO_PORT_D
+#endif
+#ifndef IADC0_SCAN1POS_PIN                      
+#define IADC0_SCAN1POS_PIN                       0
+#endif
+
 // [IADC0]$
 
 // $[ACMP0]
@@ -110,6 +182,14 @@
 // [ACMP1]$
 
 // $[VDAC0]
+// VDAC0 CH0_MAIN_OUT on PB00
+#ifndef VDAC0_CH0_MAIN_OUT_PORT                 
+#define VDAC0_CH0_MAIN_OUT_PORT                  SL_GPIO_PORT_B
+#endif
+#ifndef VDAC0_CH0_MAIN_OUT_PIN                  
+#define VDAC0_CH0_MAIN_OUT_PIN                   0
+#endif
+
 // [VDAC0]$
 
 // $[VDAC1]
@@ -122,6 +202,22 @@
 // [HFXO0]$
 
 // $[I2C0]
+// I2C0 SCL on PA05
+#ifndef I2C0_SCL_PORT                           
+#define I2C0_SCL_PORT                            SL_GPIO_PORT_A
+#endif
+#ifndef I2C0_SCL_PIN                            
+#define I2C0_SCL_PIN                             5
+#endif
+
+// I2C0 SDA on PA04
+#ifndef I2C0_SDA_PORT                           
+#define I2C0_SDA_PORT                            SL_GPIO_PORT_A
+#endif
+#ifndef I2C0_SDA_PIN                            
+#define I2C0_SDA_PIN                             4
+#endif
+
 // [I2C0]$
 
 // $[EUSART0]
@@ -134,37 +230,103 @@
 // [MODEM]$
 
 // $[CUSTOM_PIN_NAME]
-#ifndef _PORT                                   
-#define _PORT                                    SL_GPIO_PORT_A
+#ifndef LMP_VOUT_PORT                           
+#define LMP_VOUT_PORT                            SL_GPIO_PORT_A
 #endif
-#ifndef _PIN                                    
-#define _PIN                                     0
+#ifndef LMP_VOUT_PIN                            
+#define LMP_VOUT_PIN                             0
 #endif
 
+#ifndef LMP_SDA_PORT                            
+#define LMP_SDA_PORT                             SL_GPIO_PORT_A
+#endif
+#ifndef LMP_SDA_PIN                             
+#define LMP_SDA_PIN                              4
+#endif
 
+#ifndef LMP_SCL_PORT                            
+#define LMP_SCL_PORT                             SL_GPIO_PORT_A
+#endif
+#ifndef LMP_SCL_PIN                             
+#define LMP_SCL_PIN                              5
+#endif
 
+#ifndef LMP_MENB_PORT                           
+#define LMP_MENB_PORT                            SL_GPIO_PORT_A
+#endif
+#ifndef LMP_MENB_PIN                            
+#define LMP_MENB_PIN                             6
+#endif
 
+#ifndef LED_PORT                                
+#define LED_PORT                                 SL_GPIO_PORT_A
+#endif
+#ifndef LED_PIN                                 
+#define LED_PIN                                  8
+#endif
 
+#ifndef LMP_VREF_PORT                           
+#define LMP_VREF_PORT                            SL_GPIO_PORT_B
+#endif
+#ifndef LMP_VREF_PIN                            
+#define LMP_VREF_PIN                             0
+#endif
 
+#ifndef LDO_EN_PORT                             
+#define LDO_EN_PORT                              SL_GPIO_PORT_C
+#endif
+#ifndef LDO_EN_PIN                              
+#define LDO_EN_PIN                               0
+#endif
 
+#ifndef PWR_BTN_PORT                            
+#define PWR_BTN_PORT                             SL_GPIO_PORT_C
+#endif
+#ifndef PWR_BTN_PIN                             
+#define PWR_BTN_PIN                              1
+#endif
 
+#ifndef IMU_SDO_PORT                            
+#define IMU_SDO_PORT                             SL_GPIO_PORT_C
+#endif
+#ifndef IMU_SDO_PIN                             
+#define IMU_SDO_PIN                              3
+#endif
 
+#ifndef IMU_SDI_PORT                            
+#define IMU_SDI_PORT                             SL_GPIO_PORT_C
+#endif
+#ifndef IMU_SDI_PIN                             
+#define IMU_SDI_PIN                              4
+#endif
 
+#ifndef IMU_SPC_PORT                            
+#define IMU_SPC_PORT                             SL_GPIO_PORT_C
+#endif
+#ifndef IMU_SPC_PIN                             
+#define IMU_SPC_PIN                              5
+#endif
 
+#ifndef IMU_CS_PORT                             
+#define IMU_CS_PORT                              SL_GPIO_PORT_C
+#endif
+#ifndef IMU_CS_PIN                              
+#define IMU_CS_PIN                               6
+#endif
 
+#ifndef IMU_INT2_PORT                           
+#define IMU_INT2_PORT                            SL_GPIO_PORT_C
+#endif
+#ifndef IMU_INT2_PIN                            
+#define IMU_INT2_PIN                             7
+#endif
 
-
-
-
-
-
-
-
-
-
-
-
-
+#ifndef BATT_MEAS_PORT                          
+#define BATT_MEAS_PORT                           SL_GPIO_PORT_D
+#endif
+#ifndef BATT_MEAS_PIN                           
+#define BATT_MEAS_PIN                            0
+#endif
 
 // [CUSTOM_PIN_NAME]$
 
