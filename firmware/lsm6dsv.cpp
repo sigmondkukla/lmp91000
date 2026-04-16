8e66e63dbe96b2f0cabac7963063714e9e838bf3
Zachary Thornton <zacharythornton@host-128-153-80-75.host.clarkson.edu>
2026-04-16 00:15:41 -0400

    added initial lsm6dsv class structure and implementation

---
+++ firmware/lsm6dsv.cpp
@@ -0,0 +1,109 @@
+/*
+ * lsm6dsv.cpp
+ *
+ *  Created on: April 15, 2026
+ *      Author: Zachary Thornton
+ */
+
+#include "lsm6dsv.h"
+
+#include <cstddef>
+
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_eusart.h"
+#include "sl_sleeptimer.h"
+
+ void lsm6dsv::init(void){
+    spi_init();
+
+
+    if (read_reg(WHOAMI) != 0x70){
+        // failed to initialize
+    }
+    
+    // set SW_RESET
+    write_reg(CTR3, 0x01);
+    sl_sleeptimer_delay_millisecond(10); // wait for reset to complete
+
+    // set BDU
+    write_reg(CTR3, 0x40);
+
+    // configure accelerometer ODR
+    write_reg(CTR1, 0x77);
+
+    // configure gyro ODR
+    write_reg(CTR2, 0x77); // 0b01110111 for normal mode (see user guide pages 5, 17, 18)
+
+
+ }
+
+/* ------ helper function implementation ------*/
+
+ void lsm6dsv::spi_init(void){
+    EUSART_SpiInit_TypeDef init_master = EUSART_SPI_MASTER_INIT_DEFAULT_HF;
+    init_master.clockMode = eusartClockMode3;
+
+    // Enable clocks
+    CMU_ClockEnable(cmuClock_GPIO, true);
+    CMU_ClockEnable(cmuClock_EUSART1,true);
+
+    // Configure GPIO pins
+    GPIO_PinModeSet(gpio_MOSI_port, gpio_MOSI_pin, gpioModePushPull, 0);
+    GPIO_PinModeSet(gpio_MISO_port, gpio_MISO_pin, gpioModeInput,    0);
+    GPIO_PinModeSet(gpio_SCLK_port, gpio_SCLK_pin, gpioModePushPull, 0);
+    GPIO_PinModeSet(gpio_CS_port,   gpio_CS_pin,   gpioModePushPull, 1); // CS start HIGH, active LOW
+
+    // connect eusart to ports
+    GPIO->EUSARTROUTE[EUSART_NUM(EUSART1)].TXROUTE = (gpio_MOSI_port << _GPIO_EUSART_TXROUTE_PORT_SHIFT)
+       | (gpio_MOSI_pin << _GPIO_EUSART_TXROUTE_PIN_SHIFT);
+
+    GPIO->EUSARTROUTE[EUSART_NUM(EUSART1)].RXROUTE = (gpio_MISO_port << _GPIO_EUSART_RXROUTE_PORT_SHIFT)
+      | (gpio_MISO_pin << _GPIO_EUSART_RXROUTE_PIN_SHIFT);
+    
+    GPIO->EUSARTROUTE[EUSART_NUM(EUSART1)].SCLKROUTE = (gpio_SCLK_port << _GPIO_EUSART_SCLKROUTE_PORT_SHIFT)
+      | (gpio_SCLK_pin << _GPIO_EUSART_SCLKROUTE_PIN_SHIFT);
+
+    GPIO->EUSARTROUTE[EUSART_NUM(EUSART1)].ROUTEEN = GPIO_EUSART_ROUTEEN_TXPEN | GPIO_EUSART_ROUTEEN_RXPEN | GPIO_EUSART_ROUTEEN_SCLKPEN;
+    
+    // initialize the eusart
+    EUSART_SpiInit(EUSART1, &init_master);
+   
+ }
+
+ void lsm6dsv::spi_cs_assert(void){
+    GPIO_PinOutClear(gpio_CS_port, gpio_CS_pin); // active low
+ }
+
+ void lsm6dsv::spi_cs_deassert(void){
+    GPIO_PinOutSet(gpio_CS_port, gpio_CS_pin);
+ }
+
+ uint8_t lsm6dsv::spi_transfer(uint8_t data){
+    // Wait for the TXFL bit of the STATUS register to be set
+    while (!(EUSART1->STATUS & EUSART_STATUS_TXFL));
+
+    // Write the data to the TXDATA register
+    EUSART1->TXDATA = data;
+
+    // Wait for the RXFL bit of the STATUS register to be set
+    while (!(EUSART1->STATUS & EUSART_STATUS_RXFL));
+
+    // Return the data that was written to the RXDATA register
+    return EUSART1->RXDATA;
+ }
+
+ void lsm6dsv::write_reg(uint8_t reg, uint8_t data){
+    spi_cs_assert();
+    spi_transfer(reg & ~(RW));
+    spi_transfer(data);
+    spi_cs_deassert();
+ }
+
+ uint8_t lsm6dsv::read_reg(uint8_t reg){
+    spi_cs_assert();
+    spi_transfer(reg | RW);
+    uint8_t data = spi_transfer(0x00);
+    spi_cs_deassert();
+    return data;
+ }
---
+++ firmware/lsm6dsv.h
@@ -0,0 +1,82 @@
+/*
+ * lsm6dsv.h
+ *
+ *  Created on: April 15, 2026
+ *      Author: Zachary Thornton
+ */
+
+ // data sheet: https://www.st.com/resource/en/datasheet/lsm6dsv.pdf
+ // user guide: https://www.st.com/resource/en/application_note/an5922-lsm6dsv-6axis-imu-with-embedded-sensor-fusion-i3c-oiseis-for-smart-applications-stmicroelectronics.pdf
+
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_eusart.h"
+
+#ifndef LSM6DSV_LSM6DSV_H
+#define LSM6DSV_LSM6DSV_H
+
+// see page 49 of data sheet of spi-accesable register mapping
+#define WHOAMI 0x0F
+#define CTR1 0x10 // for accelerometer config
+#define CTR2 0x11 // for gyroscope config
+#define CTR3 0x12 // for software reset
+
+// read-write bit
+#define RW 0x80
+
+// #define BAUDRATE 1000000 // 1 Mhz
+
+class lsm6dsv
+{
+    public:
+        lsm6dsv(USART_TypeDef *spi_inst,
+
+                GPIO_Port_TypeDef gpio_MOSI_port, uint8_t gpio_MOSI_pin,
+                GPIO_Port_TypeDef gpio_MISO_port, uint8_t gpio_MISO_pin,
+                GPIO_Port_TypeDef gpio_SCLK_port, uint8_t gpio_SCLK_pin,
+                GPIO_Port_TypeDef gpio_CS_port,   uint8_t gpio_CS_pin,
+
+                uint32_t baudrate) : spi_inst(spi_inst),
+                                     gpio_MOSI_port(gpio_MOSI_port), gpio_MOSI_pin(gpio_MOSI_pin),
+                                     gpio_MISO_port(gpio_MISO_port), gpio_MISO_pin(gpio_MISO_pin),
+                                     gpio_SCLK_port(gpio_SCLK_port), gpio_SCLK_pin(gpio_SCLK_pin),
+                                     gpio_CS_port(gpio_SCLK_port),   gpio_CS_pin(gpio_SCLK_pin),
+
+                                     baudrate(baudrate) {};
+        ~lsm6dsv();
+
+        void init(void);
+
+
+        
+
+    private:
+        // variables
+        USART_TypeDef *spi_inst;
+
+        GPIO_Port_TypeDef gpio_MOSI_port;
+        uint8_t gpio_MOSI_pin,;
+
+        GPIO_Port_TypeDef gpio_MISO_port;
+        uint8_t gpio_MISO_pin;
+
+        GPIO_Port_TypeDef gpio_SCLK_port;
+        uint8_t gpio_SCLK_pin;
+
+        GPIO_Port_TypeDef gpio_CS_port;
+        uint8_t gpio_CS_pin;
+
+        uint32_t baudrate;
+
+        // helpers
+        void spi_init(void);
+        void spi_cs_assert(void); 
+        void spi_cs_deassert(void); 
+        uint8_t spi_transfer(uint8_t data);
+
+        void write_reg(uint8_t reg, uint8_t data);
+        uint8_t read_reg(uint8_t reg)
+        
+};
+
+#endif /* LSM6DSV_LSM6DSV_H */
