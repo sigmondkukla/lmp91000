be79f6e8a80a1ec0890ea269b258b0f6f226536d
Joseph Mire <joeymmire@gmail.com>
2026-06-08 09:28:06 -0400

    Fix Button Pressed State

--- firmware/app.cpp
+++ firmware/app.cpp
@@ -58,9 +58,10 @@ extern "C"
     // This is called once during start-up.                                    //
     /////////////////////////////////////////////////////////////////////////////
     // GPIO_PinModeSet()
+    GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 1);
+    GPIO_PinModeSet(gpioPortA, 8, gpioModePushPull, 1);
     lmp.init();
     //imu.init();
-    
   }
 
   // Application Process Action.
