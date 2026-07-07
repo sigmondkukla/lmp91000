02158d691da7e75d2c6d42f1b456338e1cdc35d8
Joseph Mire <joeymmire@gmail.com>
2026-07-07 11:49:09 -0400

    Updated updatetime.py

--- firmware/updatetime.py
+++ firmware/updatetime.py
@@ -1,22 +1,20 @@
+# Imports
 import pylink
 import time
 
+# Setup J-Link connection
 jlink = pylink.JLink()
 jlink.open(serial_no=440308528)  # Replace with your J-Link serial number
 jlink.set_tif(interface=pylink.JLinkInterfaces.SWD)
-jlink.connect(chip_name="EFR32MG24AxxxF1536", speed="auto", verbose=True)  # e.g. EFR32MG24AxxxF1536
+jlink.connect(chip_name="EFR32MG24AxxxF1536", speed="auto", verbose=True)
 
 # Start RTT
 jlink.rtt_start()
-time.sleep(1)  # Wait for RTT to initialize
+time.sleep(1)
 
 # Send your timestamp
-#jlink.rtt_write(0, b"1720224000")
-#timeStamp = 1720224000
 timeStamp = int(time.time())
 jlink.rtt_write(0, (str(timeStamp) + '\n').encode())
 
-#jlink.rtt_stop()
-#jlink.close()
-
+# Success Message
 print("Value uploaded to RTT successfully.")
